6ae7ff96065b54609543df0a35da622252094310
rg216 <rg216@outlook.com>
2024-05-01 20:03:32 +0800

    usr modification

--- user/src/bin/00hello_world.rs
+++ user/src/bin/00hello_world.rs
@@ -4,11 +4,8 @@
 #[macro_use]
 extern crate user_lib;
 
-use user_lib::get_taskinfo;
-
 #[no_mangle]
 fn main() -> i32 {
-    let app_id = get_taskinfo();
-    println!("Hello, world! taskinfo:{}", app_id);
+    println!("Hello, world!");
     0
 }
